498fdaa38638463ff5964b0d59226e66e2274297
PrincesssCharming <chantalwilkister@gmail.com>
2024-03-20 20:10:33 +0300

    day 3 question

---
+++ day3.py
@@ -0,0 +1,15 @@
+#1.  Using the variable below, print "Hello, world!".
+# greeting = "Hello, world"
+# You can add the missing exclamation mark using string concatenation, format, or f-strings. The choice is yours.
+
+# 2. Ask the user for their name, and then greet the user, using their name as part of the greeting. 
+# The name should be in title case, and shouldn't be surrounded by any excess white space.
+
+# 3. Concatenate the string "I am " and the integer 29 to produce a string which reads "I am 29".
+
+# 4. Format and print the information below using string interpolation:
+# title = "Joker"
+# director = "Todd Phillips"
+# release_year = 2019
+# The output should look like this:
+# Joker (2019), directed by Todd Phillips
