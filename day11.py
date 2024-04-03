49574d6329c5dd7f971a2b50044cc14e0a3ecf5a
PrincesssCharming <chantalwilkister@gmail.com>
2024-04-03 21:36:04 +0300

    day 11 task

---
+++ day11.py
@@ -0,0 +1,12 @@
+# 1) Create an empty set and assign it to a variable.
+
+# 2) Add three items to your empty set using either several add calls, or a single call to update.
+
+# 3) Create a second set which includes at least one common element with the first set.
+
+# 4) Find the union, symmetric difference, and intersection of the two sets. Print the results of each operation.
+
+# 5) Create a sequence of numbers using range, then ask the user to enter a number.
+# Inform the user whether or not their number was within the range you specified.
+
+# If you want an extra challenge, also tell the user if their number was too high or too low.
