798b61e37d835db79c7ceb71daffefb9ec542e3b
PrincesssCharming <chantalwilkister@gmail.com>
2024-04-09 22:34:46 +0300

    day 13 task

---
+++ day13.py
@@ -0,0 +1,19 @@
+# 1) Define a exponentiate function that takes in two numbers.
+# The first is the base, and the second is the power to raise the base to.
+# The function should return the result of this operation.
+# Remember we can perform exponentiation using the ** operator.
+
+# 2) Define a process_string function which takes in a string
+# and returns a new string which has been converted to lowercase,
+# and has had any excess whitespace removed.
+
+# 3) Write a function that takes in a tuple containing information about an actor
+# and returns this data as a dictionary. The data should be in the following format:
+
+# ("Tom Hardy", "English", 42)  # name, nationality, age
+
+# You can choose whatever key names you like for the dictionary.
+
+# 4) Write a function that takes in a single number and returns True or False depending on
+# whether or not the number is prime. If you need a refresher on
+# how to calculate if a number is prime, we show one method in day 8 of the series.
