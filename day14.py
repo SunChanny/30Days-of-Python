a5b272723653e55e0fb32a4e12aae2f7a3041528
PrincesssCharming <chantalwilkister@gmail.com>
2024-04-10 19:14:20 +0300

    day 14 task

---
+++ day14.py
@@ -0,0 +1,8 @@
+# Rewrite the following piece of code using a context manager:
+#  f = open("hello_world.txt", "w")
+#  f.write("Hello, World!")
+#  f.close()
+
+# Use append mode to write "How are you?" on the second line of the hello_world.txt file above.
+# Take the list of dictionaries we created from the Iris flower data set and write it to a new file in CSV format.
+
